70b79625a7d1a57176355067be2e683892a0ec9a
Dom Williams <me@domwillia.ms>
2021-06-19 11:11:53 +0100

    Replace zip with rc-zip in miri memory fs
    miri uses 75GB of memory and still hasn't parsed the zip file
    after 2 hours... Time for a lighter weight abstraction

--- src/classpath.rs
+++ src/classpath.rs
@@ -10,31 +10,38 @@ mod classpath_zip {
     use crate::classpath::FindClassError;
     use lazy_static::lazy_static;
     use parking_lot::Mutex;
+    use rc_zip::prelude::ReadAt;
+    use rc_zip::{Archive, EntryContents, ReadZipWithSize};
     use std::ffi::OsStr;
     use std::io::Cursor;
     use std::io::Read;
     use std::path::Path;
-    use zip::ZipArchive;
 
     const CLASSPATH_ZIP: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/classpath.zip"));
 
-    static ARCHIVE: Mutex<Option<ZipArchive<Cursor<&'static [u8]>>>> =
-        parking_lot::const_mutex(None);
+    static ARCHIVE: Mutex<Option<Archive>> = parking_lot::const_mutex(None);
 
     pub fn read_file(path: &Path) -> Option<Result<Vec<u8>, std::io::Error>> {
         let mut guard = ARCHIVE.lock();
         if guard.is_none() {
             log::debug!("parsing classpath zip");
-            *guard = Some(
-                ZipArchive::new(Cursor::new(CLASSPATH_ZIP)).expect("failed to read classpath zip"),
-            );
+            let archive: Archive = (&CLASSPATH_ZIP as &dyn ReadAt)
+                .read_zip_with_size(CLASSPATH_ZIP.len() as u64)
+                .expect("failed to read classpath zip");
+            log::debug!("done!");
+            *guard = Some(archive);
         }
 
         let archive = guard.as_mut().unwrap(); // definitely initialized now
 
-        let ret = if let Ok(mut file) = archive.by_name(&*path.to_string_lossy()) {
-            let mut bytes = Vec::with_capacity(file.size() as usize);
-            Some(file.read_to_end(&mut bytes).map(|_| bytes))
+        let ret = if let Some(entry) = archive.by_name(&*path.to_string_lossy()) {
+            let mut reader = entry.reader(|offset| {
+                let mut cursor = Cursor::new(CLASSPATH_ZIP);
+                cursor.set_position(offset as u64);
+                cursor
+            });
+            let mut bytes = Vec::with_capacity(entry.uncompressed_size as usize);
+            Some(reader.read_to_end(&mut bytes).map(|_| bytes))
         } else {
             None
         };
